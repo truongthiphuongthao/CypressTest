7a8cbf7e3b03cf42aa5072570a4aaed6e74720e0
DESKTOP-3C77M6O\pthao <thaotruong199826@gmail.com>
2023-03-01 10:05:06 +0700

    edit env

--- cypress.config.js
+++ cypress.config.js
@@ -2,12 +2,15 @@ const { defineConfig } = require("cypress");
 const cucumber = require("cypress-cucumber-preprocessor").default
 
 module.exports = defineConfig({
-
+  env: {
+    baseUrl: 'https://www.saucedemo.com',
+    userName: "standard_user",
+    password: "secret_sauce"
+  },
   e2e: {
     chromeWebSecurity: false,
     viewportWidth: 1440,
     viewportHeight: 900,
-    baseUrl: "https://www.saucedemo.com",
     setupNodeEvents(on, config) {
       on('file:preprocessor', cucumber())
     },
@@ -16,10 +19,5 @@ module.exports = defineConfig({
     pageLoadTimeout: 150000,
     defaultCommandTimeout: 5000,
     video: true,
-    env: {
-      baseUrl: 'https://www.saucedemo.com',
-      userName: "standard_user",
-      password: "secret_sauce"
-    }
   }
 });
